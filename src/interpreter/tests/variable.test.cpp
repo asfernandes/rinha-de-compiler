79b425988df7adc6ce1530bedcabb6b89697b0a3
Adriano dos Santos Fernandes <adrianosf@gmail.com>
2023-09-22 22:28:00 -0300

    More tests for variables.

--- src/interpreter/tests/variable.test.cpp
+++ src/interpreter/tests/variable.test.cpp
@@ -80,5 +80,30 @@ BOOST_AUTO_TEST_CASE(isReadCorrectlyInFnBetweenShadow)
 	BOOST_CHECK(std::get<IntValue>(result.value.value()).getValue() == 2);
 }
 
+BOOST_AUTO_TEST_CASE(readShadowedInSameDeclaration1)
+{
+	const auto result = TestUtil::run(R"###(
+		let n1 = 1;
+		let n1 = n1;
+		n1
+	)###");
+
+	BOOST_CHECK(std::get<IntValue>(result.value.value()).getValue() == 1);
+}
+
+BOOST_AUTO_TEST_CASE(readShadowedInSameDeclaration2)
+{
+	const auto result = TestUtil::run(R"###(
+		let n1 = 1;
+		let f = fn() => {
+			let n1 = n1;
+			n1
+		};
+		f()
+	)###");
+
+	BOOST_CHECK(std::get<IntValue>(result.value.value()).getValue() == 1);
+}
+
 BOOST_AUTO_TEST_SUITE_END()  // VariableSuite
 BOOST_AUTO_TEST_SUITE_END()  // InterpreterSuite
